db8eb5b6e2f5ecfa81af8e29ec49a92bbbc787e3
Devin Pastoor <devin.pastoor@gmail.com>
2015-09-21 13:00:56 -0400

    add eta_vs_cov

---
+++ R/eta_vs_cov.R
@@ -0,0 +1,21 @@
+#' create Eta vs Covariate plots
+#' @param df dataframe of eta and covariate values from pml
+#' @details
+#' expects 4 columns Covr (covariate values), Eta (eta values),
+#' CovrName (covariate name), EtaName (name of eta)
+#'
+#' can be accessed from pmldb from `.$EtaCov`
+#' @export
+eta_vs_cov <- function(df) {
+
+  ggout <- df %>%
+    dplyr::select(-(ID1:ID4)) %>%
+    ggplot(aes(x = Covr, y = Eta)) +
+    geom_point() +
+    facet_grid(CovrName~EtaName) +
+    stat_smooth(se = FALSE, size = 1.25) +
+    theme_bw() +
+    PKPDmisc::base_theme()
+
+  return(ggout)
+}
