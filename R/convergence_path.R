a2b8fb2e16adc512bfe917fd2543315e716d636b
Devin Pastoor <devin.pastoor@gmail.com>
2015-09-22 10:07:19 -0400

    update convergence to not render values for every single tick on x/y axes (due to underlying data being factor)

--- R/convergence_path.R
+++ R/convergence_path.R
@@ -3,8 +3,9 @@
 #' @export
 convergence_plot <- function(df) {
     df %>%
-        ggplot(aes(x = iteration, y = value, group = param)) +
+        ggplot(aes(x = as.numeric(iteration), y = as.numeric(value), group = param)) +
         geom_line() + geom_point() +
-        facet_wrap(~param, scales = "free_y") +
-        theme_bw() + PKPDmisc::base_theme()
+        facet_wrap(~param, scales = "free") +
+        theme_bw() + PKPDmisc::base_theme() +
+        xlab("Iteration Number") + ylab("Parameter Value")
 }
