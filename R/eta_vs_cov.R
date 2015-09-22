b8ba023a142b3c26dc2b0c4edf7e75a20cb8c96c
Devin Pastoor <devin.pastoor@gmail.com>
2015-09-22 10:02:39 -0400

    switch to facet wrap as the free scales was not working properly on facet grid but still force columns to be created so one column per eta

--- R/eta_vs_cov.R
+++ R/eta_vs_cov.R
@@ -6,14 +6,15 @@
 #' can be accessed from pmldb from `.$EtaCov`
 #' @export
 eta_vs_cov <- function(df) {
-
+ num_etas <- dplyr::n_distinct(df$EtaName)
   ggout <- df %>%
     dplyr::select(-(ID1:ID4)) %>%
     ggplot(aes(x = Covr, y = Eta)) +
     geom_point() +
-    facet_grid(CovrName~EtaName, scales = "free") +
+    facet_wrap(CovrName~EtaName, scales = "free", ncol = num_etas) +
     stat_smooth(se = FALSE, size = 1.25) +
     theme_bw() +
+      xlab("Covariate Value") + ylab("Eta Value") +
     PKPDmisc::base_theme()
 
   return(ggout)
