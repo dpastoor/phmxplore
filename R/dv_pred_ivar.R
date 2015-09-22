160a4d7cf3eaf5a2e3720987450aade8782ea2fe
Devin Pastoor <devin.pastoor@gmail.com>
2015-09-22 11:32:26 -0400

    add dv and pred vs ivar and TAD

---
+++ R/dv_pred_ivar.R
@@ -0,0 +1,38 @@
+#' dv vs ivar with overlaid pred
+#' @param df
+#' @export
+dv_pred_ivar <- function(df) {
+ ggout <- df %>%
+        dplyr::select(ID5, IVAR, PRED, DV, ObsName) %>%
+        ggplot(aes(x = IVAR, y = DV, group = ID5)) +
+        geom_line() + geom_point() +
+     geom_line(aes(y = PRED), color = "red", size = 1.25) +
+     theme_bw() +
+     PKPDmisc::base_theme() +
+     xlab("Time") + ylab("Dependent Variable")
+ if(length(unique(df$ObsName)) > 1) {
+     ggout <- ggout + facet_wrap(~ObsName)
+ }
+ return(ggout)
+
+}
+
+#' dv vs tad with overlaid pred
+#' @param df
+#' @export
+dv_pred_tad <- function(df) {
+ ggout <- df %>%
+        dplyr::select(ID5, TAD, PRED, DV, ObsName) %>%
+        ggplot(aes(x = TAD, y = DV, group = ID5)) +
+        geom_line() + geom_point() +
+     geom_line(aes(x = TAD, y = PRED), color = "red", size = 1.25) +
+     theme_bw() +
+     PKPDmisc::base_theme() +
+     xlab("Time after Dose") +
+     ylab("Dependent Variable")
+ if(length(unique(df$ObsName)) > 1) {
+     ggout <- ggout + facet_wrap(~ObsName)
+ }
+ return(ggout)
+
+}
